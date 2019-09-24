f926c06c88e3398d7276ada4d6d26ea911bb3667
Michael Karén <michael.karen@gmail.com>
2019-09-24 23:47:15 +0200

    refactor valid

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -7,17 +7,34 @@ import { COLS, ROWS, POINTS } from './constants';
 })
 export class GameService {
   valid(p: IPiece, board: number[][]): boolean {
-    return p.shape.every((row, y) => {
-      return row.every(
-        (value, x) =>
-          value === 0 ||
-          (p.x + x >= 0 &&
-            p.x + x < COLS &&
-            p.y + y < ROWS &&
-            board[p.y + y] &&
-            board[p.y + y][p.x + x] === 0)
-      );
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return (
+          this.isEmpty(value) ||
+          (this.insideWalls(x) &&
+            this.aboveFloor(y) &&
+            this.notOccupied(board, x, y))
+        );
+      });
     });
+  }
+
+  isEmpty(value: number): boolean {
+    return value === 0;
+  }
+
+  insideWalls(x: number): boolean {
+    return x >= 0 && x < COLS;
+  }
+
+  aboveFloor(y: number): boolean {
+    return y <= ROWS;
+  }
+
+  notOccupied(board: number[][], x: number, y: number): boolean {
+    return board[y] && board[y][x] === 0;
   }
 
   rotate(piece: IPiece): IPiece {
